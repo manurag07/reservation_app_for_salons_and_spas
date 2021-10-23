ba9e6dad8d1f893afc7b2736d4e9f683222b9537
Anurag Mishra <anurag1998mishra@gmail.com>
2021-10-23 14:35:33 +0530

    Initial commit

---
+++ db/migrate/20211022201517_devise_create_users.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+      t.string :company_name
+      t.string :gstin
+      t.string :pan_number
+      t.string :address
+      t.string :days_of_operations
+      t.string :hours_of_operations
+      t.integer :no_of_chairs
+      t.boolean :user_type
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
